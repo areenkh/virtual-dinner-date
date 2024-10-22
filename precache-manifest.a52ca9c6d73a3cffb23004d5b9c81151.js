5cd156882f3069272235c323540ec6ae13f29e9a
areenkh <areen.khalaila@gmail.com>
2024-10-22 12:40:48 -0400

    Updates

---
+++ precache-manifest.a52ca9c6d73a3cffb23004d5b9c81151.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ffbaf16b01ae2213b49bcc75d6a836",
+    "url": "/areenkh/virtual-dinner-date/index.html"
+  },
+  {
+    "revision": "c3afe6a26fd8f5e198bf",
+    "url": "/areenkh/virtual-dinner-date/static/css/main.498ac8cc.chunk.css"
+  },
+  {
+    "revision": "ba1aaf3230d4f39b822f",
+    "url": "/areenkh/virtual-dinner-date/static/js/2.2482cadb.chunk.js"
+  },
+  {
+    "revision": "c3afe6a26fd8f5e198bf",
+    "url": "/areenkh/virtual-dinner-date/static/js/main.25865806.chunk.js"
+  },
+  {
+    "revision": "b54eb8c2e26fb173b585",
+    "url": "/areenkh/virtual-dinner-date/static/js/runtime~main.d6764bcd.js"
+  },
+  {
+    "revision": "d4e68cce4ba0a4ccf4daf2df11482b6a",
+    "url": "/areenkh/virtual-dinner-date/static/media/angrycat.d4e68cce.gif"
+  },
+  {
+    "revision": "a3d69709008b07c99616d5c78df28bd7",
+    "url": "/areenkh/virtual-dinner-date/static/media/catplay.a3d69709.gif"
+  },
+  {
+    "revision": "169b1dc07598d7de23fc016c25b05991",
+    "url": "/areenkh/virtual-dinner-date/static/media/chickenburger.169b1dc0.jpg"
+  },
+  {
+    "revision": "66e9fdeca1137fe0d05f3e901e580f2f",
+    "url": "/areenkh/virtual-dinner-date/static/media/chickenwings.66e9fdec.jpg"
+  },
+  {
+    "revision": "c427093825d36c6e76ca91c8c80569a7",
+    "url": "/areenkh/virtual-dinner-date/static/media/cutecat.c4270938.gif"
+  },
+  {
+    "revision": "bf8d2005b7e9b04a3e0176c76b101fb7",
+    "url": "/areenkh/virtual-dinner-date/static/media/pizza.bf8d2005.jpg"
+  },
+  {
+    "revision": "33892d44ce52ea7d0a5319a304c2c207",
+    "url": "/areenkh/virtual-dinner-date/static/media/shawarma.33892d44.jpeg"
+  },
+  {
+    "revision": "8b1b7cf2872e0bb7ee272eca4c94dbc8",
+    "url": "/areenkh/virtual-dinner-date/static/media/tabouleh.8b1b7cf2.jpg"
+  },
+  {
+    "revision": "6315c254150d512c6061fabbfc626463",
+    "url": "/areenkh/virtual-dinner-date/static/media/tacos.6315c254.jpg"
+  }
+]);
