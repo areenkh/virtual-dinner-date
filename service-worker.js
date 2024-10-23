85d42278e1f50ca1909f109553dccf0f22541582
areenkh <areen.khalaila@gmail.com>
2024-10-22 22:46:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/areenkh/virtual-dinner-date/precache-manifest.116fab02589edcb80a5b736e2e9f16ae.js"
+  "/virtual-dinner-date/precache-manifest.97c1308038a8631dcf2a958ae698db8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/areenkh/virtual-dinner-date/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/virtual-dinner-date/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
